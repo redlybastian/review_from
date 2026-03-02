01f51f4f6cb0d5b37e1c73d7b599d3b4666b9f99
redlybastian <red233786@gmail.com>
2026-03-02 13:57:26 +0530

    desktop responsive

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,8 +3,8 @@
 export default function Home() {
   return (
    <section className=" w-screen h-screen">
-    <div className="w-1/2  flex flex-col items-center  justify-center mx-auto">
-        <form className="mt-10 flex flex-col gap-4" action="submit">
+    <div className="w-1/2  flex flex-col items-center  justify-center md:flex-row md:justify-start mx-auto">
+        <form className="mt-10 flex flex-col gap-4 " action="submit">
           <label className="text-xl " htmlFor="name"> 
             <input type="text" placeholder="Name"  className="w-full px-5 py-3 leading-tight border-gray-300 border-2 outline-none rounded-sm hover:rounded-xl hover:border-gray-400 focus:border-t-blue-400  focus:border-r-blue-400 focus:border-b-blue-600 focus:border-l-blue-600 focus:rounded-2xl"/>
           </label>
