cc7927f31521bcccf0bb5996985ad7dbd80ee189
redlybastian <red233786@gmail.com>
2026-03-06 15:03:48 +0530

    style

--- src/app/components/sate.tsx
+++ src/app/components/sate.tsx
@@ -3,8 +3,8 @@
 export default function UserReview(){
     return(
         <div className=" w-full h-screen mx-auto ">
-            <h3>Review Submit</h3>
-            <div className="rounded-xl pb-10 pt-5 shadow-xl flex flex-col mx-auto w-1/2 items-center justify-center bg-amber-50">
+            <h3 className="text-center text-xl font-bold">Review Submit</h3>
+            <div className="rounded-xl mt-4 pb-10 pt-5 shadow-xl flex flex-col mx-auto w-1/2 items-center justify-center bg-amber-50">
                 <form  className="flex flex-col gap-3 ">
                     <label className="text-sm font-bold" htmlFor="email">Email  </label>
                     <input className="bg-white rounded-2xl outline-1" type="email"   />
@@ -12,6 +12,7 @@ export default function UserReview(){
                     <input className="bg-white rounded-2xl outline-1" type="text"   />
                     <label className="text-sm font-bold" htmlFor="text">Review  </label>
                     <textarea name="review" id="review" rows={5} cols={5} className="bg-white rounded-2xl outline-1"></textarea>
+                    <button className="text-white bg-black px-2 py-2 rounded-4xl shadow-xl hover:bg-gray-700 cursor-pointer">Submit</button>
                 
                   
                 </form>
